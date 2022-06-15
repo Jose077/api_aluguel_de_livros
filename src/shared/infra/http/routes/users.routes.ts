d0f6e88523a1558e8b9aadef665bf17163236e01
Sandeson <josesandeson54@gmail.com>
2022-06-15 10:53:42 -0300

    :sparkles: Criado middleware de autenticacao da aplicacao

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import { AuthenticateUserController } from "@modules/accounts/useCases/AuthenticateUserController";
 import { Request, Response, Router } from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 
-usersRoutes.get("/", (req: Request, res: Response) => {
+usersRoutes.get("/", ensureAuthenticated,  (req: Request, res: Response) => {
     return res.json({message: "Hello world user! "})
 })
 
