e6eefe18620e6a7c1a0035861b2a607ba8055039
Sandeson <josesandeson54@gmail.com>
2024-03-19 14:29:04 -0300

    Corrigindo cofig babel aplicacao

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-    host: process.env.DB_HOST || "database",
+    host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || "loja_livros",
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASS || "postgres",
     type: "postgres",
     ssl: true,
-    migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
-    entities: ["./src/modules/**/entities/*.ts"],
+    migrations: [process.env.NODE_ENV == "production" ? "./dist/shared/infra/typeorm/migrations/*.js": "./src/shared/infra/typeorm/migrations/*.ts"],
+    entities: [process.env.NODE_ENV == "production" ? "./dist/modules/**/entities/*.js": "./src/modules/**/entities/*.ts"],
     cli: {
-      migrationsDir: "./src/shared/infra/typeorm/migrations"
+      migrationsDir: process.env.NODE_ENV == "production" ? "./dist/shared/infra/typeorm/migrations" : "./src/shared/infra/typeorm/migrations"
     }
 }
